bb4554426928c131e12f287931b48a1a9b74883a
Esmatt Morra <esmatt98@hotmail.com>
2023-07-06 15:37:35 +0200

    End of section 13

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -44,15 +44,19 @@ namespace API.Controllers
         [AllowAnonymous]
         [HttpPost("register")]
         public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto){
-            if (await _userManager.Users.AnyAsync(x => x.UserName == registerDto.UserName)) 
-            {
-                return BadRequest("UserName is already taken");
-            }
-
+           
              if (await _userManager.Users.AnyAsync(x => x.Email == registerDto.Email)) 
             {
-                return BadRequest("Email is already taken");
+                ModelState.AddModelError("email", "Email taken");
+                return ValidationProblem();
+            } 
+            
+            if (await _userManager.Users.AnyAsync(x => x.UserName == registerDto.UserName)) 
+            {
+                ModelState.AddModelError("username", "Username taken");
+                return ValidationProblem();
             }
+
 
             var user = new AppUser {
                 DisplayName = registerDto.DisplayName,
